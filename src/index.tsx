1e8adc99781d74e5e29c9457b869ee1d02f48f67
jaegwans <skhu201811140@gmail.com>
2023-05-05 13:51:27 +0900

    chore: redux 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import GlobalStyled from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/Theme/theme';
+import { store } from 'app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -11,8 +13,10 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-            <GlobalStyled />
-            <App />
+            <Provider store={store}>
+                <GlobalStyled />
+                <App />
+            </Provider>
         </ThemeProvider>
     </React.StrictMode>
 );
